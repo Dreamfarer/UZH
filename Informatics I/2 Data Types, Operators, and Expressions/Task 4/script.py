937b503bb8acdc6cac3c051318ca3da226d59da5
Perytron <perytron@proton.me>
2022-09-28 10:32:35 +0200

    added simpler solution to task 4 of week 2

--- Informatics I/2 Data Types, Operators, and Expressions/Task 4/script.py
+++ Informatics I/2 Data Types, Operators, and Expressions/Task 4/script.py
@@ -10,7 +10,12 @@ def transform_string():
     # the colon or the two strings before and after
     # it). Then, you can construct the final result
     # from your temporary variables.
-    res = s.split(":")[0].lower() + ":" + s.split(":")[1].upper()
+
+    # Simpler solution by Remo
+    res = s[:s.find(":")].lower() + s[s.find(":"):].upper()
+
+    # Initial solution
+    # res = s.split(":")[0].lower() + ":" + s.split(":")[1].upper()
 
     # You don't need to change the following line.
     # It simply returns the string created above.
