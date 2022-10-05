1ece714b19530c4b1769ea6ace14f2c9ce886530
Perytron <perytron@proton.me>
2022-10-05 08:55:32 +0200

    added tasks 5 from week 3

---
+++ Informatics I/3 Conditional and Iterative Statements and Testing/Task 5/script.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Height in M
+height = 10
+# Weight in KG
+weight = 10
+
+# Determine the BMI. Change the function
+# below to calculate the BMI return which category the result is in.
+# Your implementation should work with any specific value.
+# You must use the variables defined above.
+
+def get_bmi_category():
+    # You need to change the following part of the function
+    # to determine the BMI and return the correct category.
+
+    # The BMI formula is given by BMI = weight / height^2 where weight is in kilograms and height is in meters.
+
+    # For unreasonable inputs -.-
+    if height < 0 or weight < 0:
+        return "N/A"
+
+    # BMI (kg/m^2)	Category
+    # [0, 18.5]	Underweight
+    # (18.5, 25]	Normal weight
+    # (25, 30]	Pre-obesity
+    # (30, 35]	Obesity class I
+    # (35, 40]	Obesity class II
+    # (40, inf)	Obesity class III
+
+    bmi = weight / pow(height, 2)
+    bmi_category = ""
+
+    # Assign a category to the bmi index
+    if bmi >= 0 and bmi <= 18.5:
+        bmi_category = "Underweight"
+    elif bmi > 18.5 and bmi <= 25:
+        bmi_category = "Normal weight"
+    elif bmi > 25 and bmi <= 30:
+        bmi_category = "Pre-obesity"
+    elif bmi > 30 and bmi <= 35:
+        bmi_category = "Obesity class I"
+    elif bmi > 35 and bmi <= 40:
+        bmi_category = "Obesity class II"
+    elif bmi > 40:
+        bmi_category = "Obesity class III"
+    else:
+        bmi_category = "N/A"
+    
+    # Compose string like: "BMI: {26.12}, Category: Normal weight".
+    bmi_output = "BMI: " + f"{bmi:.2f}" + ", Category: " + bmi_category
+    
+    # Return the BMI values and the correct category after you have calculated the BMI.
+    return bmi_output
+
+
+# The following line calls the function and prints the return
+# value to the Console. This way you can check what it does.
+print(get_bmi_category())
