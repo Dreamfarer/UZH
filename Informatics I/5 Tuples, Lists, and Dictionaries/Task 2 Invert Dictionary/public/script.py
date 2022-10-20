8251951677c2df1a07f44cab6c49a3fd69f88f1a
Perytron <perytron@proton.me>
2022-10-20 17:50:00 +0200

    W5 T2: upload

--- Informatics I/5 Tuples, Lists, and Dictionaries/Task 2 Invert Dictionary/public/script.py
+++ Informatics I/5 Tuples, Lists, and Dictionaries/Task 2 Invert Dictionary/public/script.py
@@ -3,9 +3,26 @@
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters!
 def invert(d):
-    # implement this function
-    pass
+    '''
+    Takes a dictionary as the parameter, inverts it and returns a list
+    '''
 
+    new_dictionary = {} # Create new dictionary
+
+    # Loop over the keys of the dictionary 'd'
+    for key in d.keys():
+
+        # Check if new dictionary already has the specifc key
+        if d[key] in new_dictionary: 
+            new_dictionary[d[key]].append(key) # If it exists, add key (value of old dict.) to the value list (keys of old dict.)
+        else:
+            new_dictionary[d[key]] = [key] # Add key (value of old dict.) and add a list as a value (key of old dict.)
+
+    # Sort the values of the new dict.
+    for value_lists in new_dictionary.values():
+        value_lists.sort()
+
+    return new_dictionary
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
