88db8d82afa682779263f27a32de9002338143c5
Perytron <perytron@proton.me>
2022-10-20 17:21:13 +0200

    W5 T1: upload

--- Informatics I/5 Tuples, Lists, and Dictionaries/Task 1 Merge Lists/public/script.py
+++ Informatics I/5 Tuples, Lists, and Dictionaries/Task 1 Merge Lists/public/script.py
@@ -4,10 +4,38 @@
 # Do not rename the function or change its list of parameters!
 
 def merge(a, b):
+    '''
+    Takes two lists as parameters, merge them and return a list
+    '''
     mergelist = []
+
+    # If one or both lists are empty, the empty list should be returned.
+    if len(a) == 0 or len(b) == 0:
+        return []
+
+    # Make sure a is always larger than b
+    flipped = False # remember if we flipped a and b (important for later)
+    if len(b) > len(a):
+        a, b = b, a
+        flipped = True
+
+    # Loop over the longer list (in our case always a)
+    for index in range(len(a)):
+        
+        # Assign the first number
+        first = a[index] 
+
+        # Assign the second number
+        if index > len(b) - 1: second = b[len(b) - 1]
+        else: second = b[index]
+
+        # Flip them around if you have flipped a and b before
+        if flipped == True: first, second = second, first
+
+        # Put the variables into a new list
+        mergelist.append((first, second))
+
     return mergelist
-
-
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
