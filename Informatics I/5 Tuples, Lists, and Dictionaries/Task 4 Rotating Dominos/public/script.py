5936e62e7430af6bb8ebe12f0176ff9f2f5f3b0e
Perytron <perytron@proton.me>
2022-10-20 19:52:22 +0200

    W5 T4: upload

--- Informatics I/5 Tuples, Lists, and Dictionaries/Task 4 Rotating Dominos/public/script.py
+++ Informatics I/5 Tuples, Lists, and Dictionaries/Task 4 Rotating Dominos/public/script.py
@@ -4,8 +4,32 @@
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters!
 def min_domino_rotations(top, bottom):
-    return "..."
 
+    # Return -1 if they don't have the same length
+    if not len(top) == len(bottom): return -1
+
+    # Temporery extend the list, 'cause we need to find the number which appears the most
+    temporary_list = top + bottom
+
+    # Find the element that occurs the most
+    most_occurances = max(set(temporary_list), key = temporary_list.count) # Don't ask me: https://www.geeksforgeeks.org/python-find-most-frequent-element-in-a-list/
+
+    # Find out if we should rotate top or bottom
+    if top.count(most_occurances) <= bottom.count(most_occurances): turns = top.count(most_occurances)
+    else: turns = bottom.count(most_occurances)
+
+    # Compensate for dominos that have the same number on both sides
+    index = 0
+    while index < len(top):
+        if top[index] == bottom[index] and top[index] == most_occurances: 
+            turns -= 1
+        index += 1
+
+    # Check that one row, either the top or the bottom, has the same numbers
+    if max(top.count(most_occurances), bottom.count(most_occurances)) + turns < len(top):
+        return -1
+
+    return turns
 
 # The following line calls the function which will print # value to the Console.
 # This way you can check what it does.
