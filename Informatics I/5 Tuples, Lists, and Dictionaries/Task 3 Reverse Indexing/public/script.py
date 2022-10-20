6e97182d25710918cdd6eb18fb3b1854888fdbf8
Perytron <perytron@proton.me>
2022-10-20 18:13:25 +0200

    W5 T3: upload

--- Informatics I/5 Tuples, Lists, and Dictionaries/Task 3 Reverse Indexing/public/script.py
+++ Informatics I/5 Tuples, Lists, and Dictionaries/Task 3 Reverse Indexing/public/script.py
@@ -9,9 +9,23 @@ dataset = [
 
 def reverse_index(dataset):
     
-    index_dictionary = {}
+    index_dictionary = {} # Create new dictionary holding the the words as keys and indexes as values
 
-    # don't forget to return your resulting dictionary
+    # Loop over the dataset entries. Enumerate assigns every value an index which can be used in the loop
+    for index, dataset_entries in enumerate(dataset):
+        
+        # Each dataset might have multiple 'values', loop over all of them.
+        for str in dataset_entries.split():
+
+            str = str.lower() # Convert to lowercase
+
+            # Check if new dictionary already has the specifc key
+            if str in index_dictionary: 
+                index_dictionary[str].append(index)
+            else:
+                index_dictionary[str] = [index]
+
+    return index_dictionary
 
 # You can see the output of your function here
 print(reverse_index(dataset))
