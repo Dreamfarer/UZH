8406e1d80e2864b44edbf1377aea2403905a0c2c
decentralix <95569029+xdecentralix@users.noreply.github.com>
2022-11-30 10:27:32 +0100

    Added solution for 10.4

--- Informatics I/010 Modularization and Git/Task 4 Git Branches & Merging/public/script.sh
+++ Informatics I/010 Modularization and Git/Task 4 Git Branches & Merging/public/script.sh
@@ -6,3 +6,17 @@ echo $SOME_VAR
 
 # Add your terminal commands here. Make sure to first run them
 # locally on your machine to have more detailed error output.
+git clone $REPO_URL repo
+cd repo
+git branch feature_x
+git checkout feature_x
+printf 'print("Hello World!")' > hello.py
+git add hello.py
+git commit -m 'Add "Hello World" example'
+git push origin feature_x
+git checkout master
+printf '# good bye' > bye.py
+git add bye.py
+git commit -m 'Add "Good Bye" comment'
+git merge feature_x -m 'Merge "feature_x"'
+git push origin master
