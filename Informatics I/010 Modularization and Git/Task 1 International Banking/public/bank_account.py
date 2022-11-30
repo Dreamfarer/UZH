9d9aee1fac28dbabb5593e2d79bcb30c2a86430b
Gianluca Imbiscuso <59439327+Perytron@users.noreply.github.com>
2022-11-30 02:29:40 +0100

    Ex10: folder structure upload (#31)

---
+++ Informatics I/010 Modularization and Git/Task 1 International Banking/public/bank_account.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# add imports, if necessary
+
+class BankAccount:
+
+    def __init__(self, currency="CHF"):
+        pass
+
+    def get_currency(self):
+        pass
+
+    def get_balance(self):
+        pass
+        
+    def deposit(self, amount, currency="CHF"):
+        pass
+    
+    def withdraw(self, amount, currency="CHF"):
+        pass
