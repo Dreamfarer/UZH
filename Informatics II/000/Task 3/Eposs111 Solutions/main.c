0bf58bb23d19425ca90a8bf8e9b02aacdd32b4ab
Eposs111 <117736553+Eposs111@users.noreply.github.com>
2023-02-23 16:04:19 +0100

    Add Solution to Task 3 (#73)
    * Add Solution to Task 3
    
    * Add Solution to Task 3
    
    * Corrected a mistake in Pseudocode

---
+++ Informatics II/000/Task 3/Eposs111 Solutions/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+
+int A[] = {2, 45, 16, 2, 6, 33, 34};
+
+int main() {
+    int len = sizeof(A)/sizeof(A[0]);
+    int largest = INT_MIN;
+    int second = INT_MIN;
+
+    for (int i = 0; i < len; i++){
+        if (A[i]> largest){
+                   second = largest;
+                   largest = A[i];
+        }
+        else if (A[i] > second){
+            second = A[i];
+        }
+
+    }
+    printf("Second largest number of A: %d \n", second);
+}
+
+
