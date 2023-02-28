6ff12cb37f4f5274baf80c7a09a2fa8bfdfa40a5
Noah Bussinger <development@palto.ch>
2023-02-28 14:23:28 +0100

    Fix 001 task 2 solution

--- Informatics II/001/Task 2/C0DECYCLE Solution/task2.c
+++ Informatics II/001/Task 2/C0DECYCLE Solution/task2.c
@@ -3,15 +3,25 @@
 #define DIM 2
 
 int main() {
-    int i, x, y;
+    int i, x, y, k;
     int mat[DIM * DIM];
     int matSqr[DIM * DIM];
 
     for (i = 0; i < DIM * DIM; i++) {
         scanf("%d", &mat[i]);
     }
+
     for (i = 0; i < DIM * DIM; i++) {
         matSqr[i] = mat[i] * mat[i];
+    }
+
+    for (i = 0; i < DIM * DIM; i++) {
+        x = i % DIM;
+        y = (i - x) / DIM;
+        matSqr[i] = 0;
+        for (k = 0; k < DIM; k++) {
+            matSqr[i] += mat[y * DIM + k] * mat[k * DIM + x];
+        }
     }
 
     for (y = 0; y < DIM; y++) {
