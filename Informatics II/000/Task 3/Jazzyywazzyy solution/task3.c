ecd96d98cb619bcf71f91d4763f2b0926c638381
jazzyywazzyy <103603393+jazzyywazzyy@users.noreply.github.com>
2023-03-17 15:43:47 +0100

    Info2-000-03: Add solution  (#101)
    Info2-000-03: Add Jazzyywazzyy solution

---
+++ Informatics II/000/Task 3/Jazzyywazzyy solution/task3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int max_num, second_max; 
+    int nums[] = {7,4,3,2,8}; 
+    int array_len = sizeof(nums)/sizeof(nums[0]);
+
+    if (nums[0] > nums[1]){
+        max_num = nums[0]; 
+        second_max = nums[1];
+    }else{
+        max_num = nums[1]; 
+        second_max = nums[0]; 
+    }
+
+    for(int i = 2; i < array_len; i++){
+        if (max_num < nums[i]){
+            second_max = max_num; 
+            max_num = nums[i]; 
+        }else if(second_max < nums[i]){
+            second_max = nums[i]; 
+        }
+    }
+    printf("Second largest number: %d", second_max); 
+}
+    
+
+ 
