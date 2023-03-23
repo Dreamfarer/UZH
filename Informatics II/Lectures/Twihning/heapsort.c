c956fefc9c1f9251e782416a3147687752532a21
Leon Braga <leonroberto.braga@uzh.ch>
2023-03-23 13:30:43 +0100

    Info2-lectures: apply requested changes

--- Informatics II/Lectures/Twihning/heapsort.c
+++ Informatics II/Lectures/Twihning/heapsort.c
@@ -2,8 +2,6 @@
 #include <math.h>
 #define N 13
 
-int arr[N] = {0,2,7,8,3,9,4,1,6,11,14,12,17};
-
 //function to swap 2 elements
 void swap(int *a, int i, int j){
         int temp = a[i];
@@ -21,51 +19,53 @@ int left(int i){
 }
 
 //convert the tree to a heap from the index i downwards
-void heapify(int *A, int i , int k){
+void heapify(int *a, int i , int k){
     int m, l, r;
     m = i;
     l = left(i);
     r = right(i);
 
     //compare parent to children and remeber index if parent is smaller
-    if (l<k && A[l] > A[m]){
+    if (l<k && a[l] > a[m]){
         m = l;
     }
-    if (r<k && A[r] > A[m]){
+    if (r<k && a[r] > a[m]){
         m = r;
     }
     
     //swap bigger child with parent and call heapify with the new child as start index
     if(i!=m){
-        swap(A, i, m);
-        heapify(A, m, k);
+        swap(a, i, m);
+        heapify(a, m, k);
     }
 }
 
 //iterate through all parent nodes and heapify 
-void Build_heap(int *A,int l){
-    for (int i = (l/2)-1; i>=0; i--){
-        heapify(A,i,l);
+void Build_heap(int *a,int k){
+    for (int i = (k/2)-1; i>=0; i--){
+        heapify(a,i,k);
     }
 }
 
-void Heap_sort(int *A, int p){
-    int s = p;
+void Heap_sort(int *a, int k){
+    int s = k;
     //convert to a heap
-    Build_heap(A, p);
+    Build_heap(a, k);
     //biggest element must be at the top therefore you can swap it to the end and call heapify
-    //on the list that is one element shorter then before so the bigest element in the back
+    //on the list that is one element shorter than before so the biggest element in the back
     //doesn't get touched
-    for (int i = 0; i < p; i++){
-        swap(A, 0, p-1-i);
+    for (int i = 0; i < k; i++){
+        swap(a, 0, k-1-i);
         s--;
-        heapify(A,0,s);
+        heapify(a,0,s);
     //now 2. biggest element is at the top
     }
 }
 
 
 int main(){
+
+    int arr[N] = {0,2,7,8,3,9,4,1,6,11,14,12,17};
 
     Heap_sort(arr, N);
 
