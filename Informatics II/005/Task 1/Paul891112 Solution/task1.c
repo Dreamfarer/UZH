68779f3b6381c5a70921523c6c634361e3e8284b
Paul TC Huang <paul891112@hotmail.com>
2023-03-26 18:51:08 +0200

    Info2-005-01-02: Add solution

---
+++ Informatics II/005/Task 1/Paul891112 Solution/task1.c
@@ -0,0 +1,166 @@
+#include <stdio.h>
+
+
+
+
+
+
+void printHeap(int A[], int n, int d){
+    //prints the created max-heap to the console in
+//the format graph g f (all the edges in the form NodeA -- NodeB) g, where each edge should
+//be printed in a separate line. The ordering of the edges is not relevant.
+
+    if(n<2){return;} //heap with one or zero element has no children, dont print
+
+    printf("g { \n");
+
+    for(int i=0;i<=n/d;i++){
+        for (int j=d*i+1;j<=(i+1)*d && j<n;j++){
+            printf("\t%d -- %d \n", A[i],A[j]);
+        }
+        
+
+    }
+    printf("}\n\n");
+}
+
+
+
+void heapify(int A[], int i, int s, int d){
+    //build heap structure of size s (not index) out of array A at node i, d = children of a node
+    
+    
+    int child = 3*i+1; //index of first child
+    int m=i; //placeholder for index of biggest elem
+    for (int j=0;j<d;j++){ //go thru every child (j just to count)
+        if (A[m]<A[child] && child<s){
+            m=child; //remember the larger elem within heap range
+
+        }
+        child++; //increment child index
+    }
+
+    if (i != m){ //swap if a larger elem exists
+        int temp = A[i];
+        A[i] = A[m];
+        A[m] = temp;
+        heapify(A,m,s,d); //restore heap at the lower level node after swapping
+    }
+    printHeap(A,s,d);
+
+}
+
+
+
+void buildMaxHeap(int A[], int n, int d);
+//A is the array to be converted, n
+//is the real size of the array A and d is the maximum number of child each node can have in the
+//heap.
+
+void heapSort(int A[], int n, int d); //sorts in ascending order
+
+void printArray(int A[], int n){//prints a given array to the console
+
+    printf("Array: {");
+
+    for(int i=0;i<n;i++){
+        printf("%d, ", A[i]);
+    }
+
+    printf("}\n\n");
+
+}
+
+
+
+void buildMaxHeap(int A[], int n, int d){
+    
+    for (int i=(n-2)/d;i>=0;i--){ //c=(n-1) is the index of last child, (c-1)/d is the parent index of any c, so (n-2)/d is the largest parent index for heap of size n
+
+        heapify(A,i,n,d);
+
+    }
+    printHeap(A,n,d);
+
+}
+
+
+
+void heapSort(int A[], int n, int d){
+    
+    //start with a maxheap
+    buildMaxHeap(A,n,d);
+    
+
+
+    for (int i=n-1;i>=0;i--){
+        
+        int temp = A[i];
+        A[i]=A[0];
+        A[0]=temp;
+        heapify(A,0,i,d);
+        
+
+    }
+
+    
+    
+
+}
+
+int main(){
+
+
+
+    //modify test input here
+
+    //n=size of array, number of elements
+    int n= 10;
+
+    //A[] the actual array to be sorted
+    int A[]={4,3,2,5,6,7,8,9,12,1};
+
+    //d-ary heap, number of maximal children of each node
+    int d = 3;
+
+
+    //call heapSort and print the final result
+    heapSort(A,n,d);
+    printArray(A,n);
+    
+
+}
+
+/* First attempt of buildMaxHeap, without heapify (works)
+
+void buildMaxHeap(int A[], int n, int d){
+    
+    for (int i=(n-2)/d;i>=0;i--){ //c=(n-1) is the index of last child, (c-1)/d is the parent index of any c, so (n-2)/d is the largest parent index for heap of size n
+
+        //i is parent index with children, starts at the largest
+        int m=i; 
+        int nextnode = i*d+1; //index of first child
+        
+        for (int j=0;j<d;j++){ //go through all d children of current node
+            if(A[m]<A[nextnode] && nextnode < n){//if larger elem is found in children and we are in range of heap
+
+                m = nextnode; //remember the index of larger elem
+            } 
+            nextnode++; //increment nextnode to get next child
+        }
+
+        if (i != m){
+            //swap if a larger child is found
+            int tempo = A[m];
+            A[m] = A[i];
+            A[i]=tempo;
+
+
+            buildMaxHeap(A,n,d);
+
+        }
+
+    }
+    printHeap(A,n,d);
+
+}*/
---
+++ Informatics II/005/Task 2/Paul891112 Solution/task2.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int higherHoarePartition(int A[], int l, int r){
+    //Exact same Hoare Partition as in lecture
+
+    int pivotj = A[r]; //pivot for j
+    
+    
+    
+    int j = r+1; int jj = l-1;
+
+    while (1){
+        j--;
+        while(A[j]>pivotj){
+            j--;
+        }
+        jj++;
+        while(A[jj]<pivotj){
+            jj++;
+        }
+
+        if(jj<j){
+            //swap
+            int temp = A[jj];
+            A[jj] = A[j];
+            A[j] = temp;
+
+        }
+        else{
+            return jj;
+        }
+
+    }
+}
+
+int lowerHoarePartition(int A[], int l, int r){
+    //Hoare partition using left most element as pivot, first increment i from left, decrement ii from right, in the end return ii which is equivalent to i in lecture
+
+    int pivoti = A[l]; //pivot for i
+
+
+    int i=l-1; int ii = r+1;
+
+    while (1){
+        i++;
+        while(A[i]<pivoti){
+            i++;
+        }
+        ii--;
+        while(A[ii]>pivoti){
+            ii--;
+        }
+
+        if(i<ii){
+            //swap
+            int temp = A[ii];
+            A[ii] = A[i];
+            A[i] = temp;
+
+        }
+        else{
+            return ii;
+        }
+
+    }
+}
+
+
+void dpQuicksort(int A[], int l, int r){
+
+    if(l>=r){
+        return;
+    }
+
+    //assert left pivot < right pivot before sorting begins
+    if (A[r] < A[l]){
+        int t = A[r];
+        A[r] = A[l];
+        A[l] = t;
+    }
+
+    int n = higherHoarePartition(A,l,r);
+    int m = lowerHoarePartition(A,l,r); 
+
+
+    dpQuicksort(A,l,m);
+    dpQuicksort(A,m+1,n-1);
+    dpQuicksort(A,n,r);
+
+}
+
+
+
+
+int main(){
+
+    //Konfigure array elements and size here, size = number of elements
+    int A[] = {10,7,3,15,6,2,5,1,17,8,14,19,3,5,0};
+    int size = 15;
+
+
+
+    printf("Before: [ ");
+    for (int i= 0;i < size; i++){
+        printf("%d ", A[i]);
+    }
+    printf("]\n");
+
+
+    dpQuicksort(A,0,size-1);
+
+
+    printf("After: [ ");
+    for (int i= 0;i < size; i++){
+        printf("%d ", A[i]);
+    }
+    printf("]\n");
+
+}
