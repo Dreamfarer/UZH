1f02530d99079943165d57d35be7ccaf12b2c1de
Noah Bussinger <development@palto.ch>
2023-04-03 12:52:33 +0200

    Info2-006-02: Fix solution

--- Informatics II/006/Task 2/C0DECYCLE Solution/task2.c
+++ Informatics II/006/Task 2/C0DECYCLE Solution/task2.c
@@ -12,18 +12,16 @@ void print(const int* arr, int n) {
 int* reverse(const int* arr, int n) {
     int* reversed = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
-        reversed[i] = *(arr + n - (i + 1));
+        *(reversed + i) = *(arr + n - (i + 1));
     }
     return reversed;
 }
 
-int* prepend(const int* arr, int v) {
-    int* prepended = malloc(sizeof(int) + sizeof(arr));
+int* prepend(const int* arr, int n, int v) {
+    int* prepended = malloc((n + 1) * sizeof(int));
     *(prepended) = v;
-    int i = 0;
-    while (i * sizeof(int) <= sizeof(int) + 2 * sizeof(arr)) {
-        *(prepended + i + 1) = arr[i];
-        i++;
+    for (int i = 0; i < n; i++) {
+        *(prepended + (i + 1)) = *(arr + i);
     }
     return prepended;
 }
@@ -31,7 +29,7 @@ int* prepend(const int* arr, int v) {
 int main() {
     int* arr = malloc(N * sizeof(int));
     for (int i = 0; i < N; i++) {
-        arr[i] = i;
+        *(arr + i) = i;
     }
     printf("The original: ");
     print(arr, N);
@@ -41,7 +39,7 @@ int main() {
     printf("\nThe reversed: ");
     print(reversed, N);
 
-    int *prepended = prepend(reversed, 5);
+    int *prepended = prepend(reversed, N, 5);
     free(reversed);
     printf("\nThe prepended: ");
     print(prepended, N + 1);
