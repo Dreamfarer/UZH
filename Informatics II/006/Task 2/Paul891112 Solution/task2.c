c1e4bd665f4d09498d487c02f93fecad892934b4
Paul TC Huang <paul891112@hotmail.com>
2023-04-04 09:43:25 +0200

    Info2-006-02-03: Add solution
    add .c files

---
+++ Informatics II/006/Task 2/Paul891112 Solution/task2.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 5
+
+
+void print(int *arr, int n);
+
+int* reverse(int *arr, int n);
+
+int* prepend(int *arr, int v);
+
+
+void print(int *arr, int n){
+    printf("%d", arr[0]);
+    for(int i=1;i<n;i++){
+        printf(", %d", arr[i]);
+    }
+    printf("\n");
+}
+
+
+int* reverse(int *arr, int n){
+
+    int* p = malloc(n*sizeof(int));
+    for(int i=0;i<n;i++){
+        p[i] = arr[n-1-i];
+    }
+    return p;
+
+}
+
+
+int* prepend(int *arr, int v){
+    int* p = arr-1;
+    p[0] = v;
+    return p;
+}
+
+int main(){
+
+    int *arr;
+    arr = malloc (N*sizeof(int));
+
+    for(int i=0;i<N;i++){
+        arr[i] = i;
+    }
+
+    printf("The original: ");
+    print(arr, N);
+
+    int *reversed = reverse(arr, N);
+
+    free(arr);
+
+    printf("The reversed: ");
+    print(reversed, N);
+
+    int *prepended = prepend(reversed, 5);
+
+    free(reversed);
+
+    printf("The prepended: ");
+    print(prepended, N+1);
+
+}
+
