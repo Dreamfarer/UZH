cf551baebd7b48d8808dc1e2bca65839c390b7b3
Patertuck <115107324+Patertuck@users.noreply.github.com>
2023-04-08 11:42:18 +0200

    Update Task2.c

--- Informatics II/006/Task 2/Patertuck Solution/Task2.c
+++ Informatics II/006/Task 2/Patertuck Solution/Task2.c
@@ -20,19 +20,19 @@ void print(int *arr, int n){
 int* reverse(int *arr, int n){
     int* temp = malloc(N * sizeof(int));
 
-    for (int i = 0; i < N; i++){
-        *(temp + i) = *(arr + N - 1 - i); 
+    for (int i = 0; i < n; i++){
+        *(temp + i) = *(arr + n - 1 - i); 
     }
 
     return temp;  
 }
 
 // C
-int* prepend(int *arr, int v){
-    int* temp = malloc((N + 1) * sizeof(int));
+int* prepend(int *arr, int v, int n){
+    int* temp = malloc((n + 1) * sizeof(int));
     *temp = v;
 
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < n; i++){
         *(temp + i + 1) = *(arr + i);
     }
 
@@ -57,7 +57,7 @@ int main() {
     printf("The reversed: ");
     print(reversed, N);
 
-    int *prepended = prepend(reversed, 5);
+    int *prepended = prepend(reversed, 5, N);
 
     free(reversed);
 
